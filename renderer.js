51001495e36c1c2c62b2dc13212ecbfe20b3bde7
Thiago Rodrigues Vasconcellos <thiagorova@gmail.com>
2018-05-28 17:15:19 -0300

    error

--- renderer.js
+++ renderer.js
@@ -1,3 +1,11 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+const { remote } = require('electron'),
+    dialog = remote.dialog;
+dialog.showOpenDialog(remote.getCurrentWindow(), {
+      properties: ['openFile',],
+      title: "Please select your audio file",
+      filters: [
+        { name: 'All files', extensions: ['mp3', 'wav', 'flac'] },
+      ]}, () => {})
